d47bfbcc246b3fd0c2dd32576852b73032319f16
oreqizer <oreqizer@gmail.com>
2019-11-26 10:46:47 +0100

    Propagate

--- src/work.cpp
+++ src/work.cpp
@@ -1,9 +1,10 @@
 #include <random>
 #include "work.h"
+#include "sigmoid.h"
 #include "enums.h"
 
 template<int N>
-void randomize(vector<Connection<N>> &conns) {
+void work::randomize(vector<Connection<N>> &conns) {
     random_device rd;
     mt19937 mt(rd());
     uniform_real_distribution<float> dist(-EPSILON_INIT, EPSILON_INIT);
@@ -15,3 +16,36 @@ void randomize(vector<Connection<N>> &conns) {
         }
     }
 }
+
+template void work::randomize(vector<Connection<LAYER_IN_BIAS>> &conns);
+
+template void work::randomize(vector<Connection<LAYER_HIDDEN_BIAS>> &conns);
+
+void work::propagate(vector<float> &neurons, Image &image) {
+    int i = 1;
+    int size = neurons.size();
+    while (i++ < size) {
+        neurons[i] = image.pixels[i];
+    }
+    neurons[0] = 1; // Bias
+}
+
+template<int N>
+void work::propagate(vector<float> &prevN, vector<Connection<N>> &conns, vector<float> &currN) {
+    int i = 0;
+    for (auto &c: conns) {
+        c.z = 0; // Reset from previous iterations
+
+        int j = 0;
+        while (j++ < N) {
+            c.z += prevN[j] * c.weights[j];
+        }
+        currN[i] = sigmoid::classic(c.z);
+
+        i += 1;
+    }
+}
+
+template void work::propagate(vector<float> &prevN, vector<Connection<LAYER_IN_BIAS>> &conns, vector<float> &currN);
+
+template void work::propagate(vector<float> &prevN, vector<Connection<LAYER_HIDDEN_BIAS>> &conns, vector<float> &currN);
--- src/work.h
+++ src/work.h
@@ -3,12 +3,18 @@
 
 #include <vector>
 #include "Connection.h"
+#include "image.h"
 
 using namespace std;
 
 namespace work {
     template<int N>
     void randomize(vector<Connection<N>> &conns);
+
+    void propagate(vector<float> &neurons, Image &image);
+
+    template<int N>
+    void propagate(vector<float> &prevN, vector<Connection<N>> &conns, vector<float> &currN);
 }
 
 #endif //MNIST_WORK_H
